84434b955a00c9bcb5e3a7eaa5831479afc1793f
Zeppelinpp <1056118649@qq.com>
2026-03-07 14:16:14 +0800

    tree sitter config

---
+++ nvim/lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = {
+      textobjects = {
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+          },
+        },
+      },
+    },
+  },
+}
