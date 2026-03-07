76a58e8dd7cd23ebda30dcd69428280bdf4bb8c7
Zeppelinpp <1056118649@qq.com>
2026-03-07 14:16:03 +0800

    add osc52 clipboard tool config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,19 @@
 
 -- Custom settings from previous config
 vim.opt.clipboard = "unnamedplus"
+
+-- OSC 52 clipboard for remote SSH sessions
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.spelllang = { "en", "cjk" }
