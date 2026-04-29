96209fb07e1853738acdeb990b343312f22a5b54
Zeppelinpp <1056118649@qq.com>
2026-04-29 16:54:14 +0800

    update tmux & nvim config for remote

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,19 +8,6 @@ vim.g.lazyvim_cmp = "blink.cmp"
 vim.opt.timeoutlen = 800 -- 增加 leader 键等待时间（毫秒）
 vim.opt.autoread = true
 vim.opt.clipboard = "unnamedplus"
-
--- OSC 52 clipboard for remote SSH sessions
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.spelllang = { "en", "cjk" }
